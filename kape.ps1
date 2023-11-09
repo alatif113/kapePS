05db1798c9a127a33c92b62e1c868e6528aab69c
Mohammed Latif <alatif113@gmail.com>
2023-11-08 22:51:16 -0500

    Update kape.ps1

--- kape.ps1
+++ kape.ps1
@@ -97,6 +97,7 @@ param(
 
 $global:ROOT = "C:\"                                                                                    # Root drive
 $global:KAPE_WORKING_PATH = [System.IO.Path]::Combine($ROOT, "KAPE")                                    # Working directory
+$global:LOGGING_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "kape_powershell.log")             # Log file
 $global:KAPE_INSTALL_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "kape-master")                # KAPE package directory
 $global:KAPE_ARCHIVE_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "kape.zip")                   # Temporary name for KAPE zip file 
 $global:KAPE_VERSION_PATH = [System.IO.Path]::Combine($KAPE_INSTALL_PATH, "version")                    # Path to KAPE version file
@@ -106,23 +107,36 @@ $global:7Z_EXE_PATH = [System.IO.Path]::Combine($KAPE_INSTALL_PATH, "7za.exe")  
 $global:KAPE_TARGETS_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "targets")                    # Directory to store KAPE target outupts
 $global:KAPE_MODULES_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "modules")                    # Directory to store KAPE module outputs
 $global:KAPE_ALL_OUTPUTS_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "outputs")                # KAPE outputs
-$global:AZCOPY_TEST_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "azcopy_test_file.txt")    # File to test azcopy
+$global:AZCOPY_TEST_PATH = [System.IO.Path]::Combine($KAPE_WORKING_PATH, "azcopy_test_file.txt")        # File to test azcopy
+$global:MAX_LOG_SIZE_KB = 1048576                                                                       # Log file bytes threshold before rotating
 
 #########################################
 # Functions
 #########################################
 
+# Write a log to a log file and the console simultaneously
 function WriteLog {
-    param(
-        [ValidateSet('Info', 'Warn', 'Error', IgnoreCase = $false)]
-        [string]$Severity = "Info",
-        [Parameter(Mandatory = $true)]
-        [string]$Message
-    )
-    
-    Write-Host "$(Get-Date) $Severity $Message"
+    param($Severity, $Message)
+    $ProcessID = [System.Diagnostics.Process]::GetCurrentProcess().Id
+    "$(Get-Date) $Severity [$ProcessID] $Message" | Tee-Object -FilePath $LOGGING_PATH -Append | Write-Host
 }
 
+# Check if log file exceeds size threshold, and rotate file if so
+function RotateLog {
+    if (-not(Test-Path $LOGGING_PATH)) {
+        return
+    }
+
+    $Log = Get-Item $LOGGING_PATH
+    if ($Log.Length -ge $MAX_LOG_SIZE_KB) { 
+        WriteLog -Severity "Info" -Message "Log file is larger than $MAX_LOG_SIZE_KB KB"
+        $NewName = "$($Log.BaseName)_$(Get-Date -Format "yyyyMMdd").log"
+        Rename-Item -Path $LOGGING_PATH -NewName $NewName -Force
+        WriteLog -Severity "Info" -Message "Rotated file to $NewName" 
+    }
+}
+
+# Download KAPE from remote repository
 function GetKAPE {
     # Delete any existing KAPE binaries
     if ($(Test-Path -Path $KAPE_INSTALL_PATH)) {
@@ -135,8 +149,8 @@ function GetKAPE {
     try {
         Invoke-WebRequest -Uri $RepoLocation -OutFile $KAPE_ARCHIVE_PATH
     } catch {
-        WriteLog -Severity "Error" -Message "Error downloading KAPE to to $KAPE_ARCHIVE_PATH; $_"
-        exit 1
+        WriteLog -Severity "Error" -Message "Failed to download KAPE to to $KAPE_ARCHIVE_PATH; $_"
+        exit
     }
     WriteLog -Severity "Info" -Message "Downloaded KAPE to to $KAPE_ARCHIVE_PATH"
 
@@ -145,47 +159,31 @@ function GetKAPE {
     try {
         Expand-Archive -Path $KAPE_ARCHIVE_PATH -DestinationPath $KAPE_WORKING_PATH -Force
     } catch {
-        WriteLog -Severity "Error" -Message "Error extracting KAPE to $KAPE_WORKING_PATH; $_"
-        exit 1
+        WriteLog -Severity "Error" -Message "Unable to extract KAPE to $KAPE_WORKING_PATH; $_"
+        exit
     }
     WriteLog -Severity "Info" -Message "Extracted KAPE to $KAPE_INSTALL_PATH"
     Remove-Item -Path $KAPE_ARCHIVE_PATH
-}
-
-function TestAzCopy {
-    $Return = $true
-    New-Item -ItemType File -Path $AZCOPY_TEST_PATH | Out-Null
-    $Output = & $AZCOPY_EXE_PATH copy $AZCOPY_TEST_PATH https://$StorageAccount.blob.core.windows.net/$StorageContainer/$StorageToken
-    
-    if ($LASTEXITCODE -eq 1) {
-        WriteLog -Severity "Error" -Message "Error running azcopy test; try storing KAPE outputs locally instead, exiting."
-        $Return = $false
-    } 
-
-    if ($Output -match 'AuthenticationFailed') {
-        WriteLog -Severity "Error" -Message "AuthenticationFailed error running azcopy test; try checking if the SAS token is still valid, exiting."
-        $Return = $false
-    }
-
-    Remove-Item $AZCOPY_TEST_PATH
-    return $Return
 }
 
 #########################################
 # Main
 #########################################
 
+# Check if log file is too big. If so, rotate
+RotateLog
+
 # Check for Admin privileges
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (-not($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
-    WriteLog -Severity "Error" -Message "Administrative privileges are required to run KAPE, exiting."
-    exit 1
+    Write-Host "$(Get-Date) Error [$([System.Diagnostics.Process]::GetCurrentProcess().Id)] Administrative privileges are required to run KAPE, exiting."
+    exit
 }
 
 # Check if Root directory exists
 if(-not($(Test-Path $ROOT))) {
-    WriteLog -Severity "Error" -Message "Root directory $ROOT not found, exiting."
-    exit 1
+    Write-Host "$(Get-Date) Error [$([System.Diagnostics.Process]::GetCurrentProcess().Id)] Root directory $ROOT not found, exiting."
+    exit
 }
 
 # Disable progress bars from writing to the standard output
@@ -193,9 +191,16 @@ $global:ProgressPreference = 'SilentlyContinue'
 
 # Create working directory
 if (Test-Path $KAPE_WORKING_PATH) {
-    WriteLog -Severity "Info" -Message "No need to create a temp working directory. $KAPE_WORKING_PATH already exists."
+    WriteLog -Severity "Info" -Message "########################## kape.ps1 PowerShell session started ##########################"
+    WriteLog -Severity "Info" -Message "No need to create a working directory. $KAPE_WORKING_PATH already exists."
 } else {
-    New-Item -ItemType Directory -Force -Path $KAPE_WORKING_PATH | Out-Null
+    try {
+        New-Item -ItemType Directory -Force -Path $KAPE_WORKING_PATH | Out-Null
+    } catch {
+        Write-Host "$(Get-Date) Error [$([System.Diagnostics.Process]::GetCurrentProcess().Id)] Unable to create $KAPE_WORKING_PATH, exiting."
+        exit
+    }
+    WriteLog -Severity "Info" -Message "########################## kape.ps1 PowerShell session started ##########################"
     WriteLog -Severity "Info" -Message "Created working directory $KAPE_WORKING_PATH."
 }
 
@@ -212,17 +217,43 @@ elseif ($PSBoundParameters.ContainsKey('Version')) {
         GetKape
     }
 } else {
-    WriteLog -Severity "Info" -Message "Using existing installation of KAPE."
+    WriteLog -Severity "Info" -Message "Using existing installation of KAPE, version $(Get-Content -Path $KAPE_VERSION_PATH)."
 }
 
 # Test azcopy
 if($PSBoundParameters.ContainsKey('StorageAccount')) {
     WriteLog -Severity "Info" -Message "Azure storage account set, testing upload to https://$StorageAccount.blob.core.windows.net/$StorageContainer."
-    if(TestAzCopy) {
-        WriteLog -Severity "Info" -Message "Successfully tested connection to https://$StorageAccount.blob.core.windows.net/$StorageContainer."
-    } else {
-        exit 1
+    
+    # Create small test file to upload
+    New-Item -ItemType File -Path $AZCOPY_TEST_PATH -Force | Out-Null
+
+    # Attempt upload as a background job with a 60 second timeout and check output for errors
+    $Job = Start-Job -ScriptBlock {
+        & $using:AZCOPY_EXE_PATH copy "$using:AZCOPY_TEST_PATH" "https://$using:StorageAccount.blob.core.windows.net/$using:StorageContainer/$using:StorageToken"
     }
+    $Job | Wait-Job -Timeout 60 | Out-Null
+    $State = $Job.State
+
+    if ($State -eq 'Running') {
+        $Job | Stop-Job
+        WriteLog -Severity "Error" -Message "Connection timeout to https://$StorageAccount.blob.core.windows.net/$StorageContainer, exiting."
+        exit
+    } elseif ($State -eq 'Failed') {
+        $ErrorString = $($Job.ChildJobs[0].JobStateInfo.Reason.Message)
+    } elseif ($State -eq 'Completed') {
+        $ErrorString = $Job | Receive-Job | Select-String -Pattern 'RESPONSE 403:|error' | Select-Object -First 1
+    }
+
+    # Delete small test file
+    Remove-Item $AZCOPY_TEST_PATH -Force
+
+    # If errors, log and exit
+    if ($null -ne $ErrorString) {
+        WriteLog -Severity "Error" -Message "Connection test to https://$StorageAccount.blob.core.windows.net/$StorageContainer failed; $ErrorString."
+        exit
+    }
+
+    WriteLog -Severity "Info" -Message "Connection test successful."
 }
 
 # Create argument string to kape.exe
@@ -270,51 +301,96 @@ $ScriptBlockParams = [PSCustomObject]@{
     ZipExePath = $7Z_EXE_PATH
     KapeTargetsPath = $KAPE_TARGETS_PATH
     KapeModulesPath = $KAPE_MODULES_PATH
+    LoggingPath = $LOGGING_PATH
 }
 
 # Define script block for Job
 $ScriptBlock = {
     param($ScriptBlockParams)
+
+    function WriteLog {
+        param($Severity, $Message)
+        $ProcessID = [System.Diagnostics.Process]::GetCurrentProcess().Id
+        "$(Get-Date) $Severity [$ProcessID] $Message" | Tee-Object -FilePath $($ScriptBlockParams.LoggingPath) -Append | Write-Host
+    }
     
     # Start KAPE
-    & $ScriptBlockParams.KapeExePath $ScriptBlockParams.KapeArgs
+    try {
+        & $ScriptBlockParams.KapeExePath $ScriptBlockParams.KapeArgs
+    } catch {
+        WriteLog -Severity "Error" -Message "$_."
+        Throw "$_"
+        exit
+    }
 
     # If Targets directory not created, something went wrong; exit
     if (-not($(Test-Path $ScriptBlockParams.KapeTargetsPath))) {
-        exit 1
+        WriteLog -Severity "Error" -Message " KAPE did not run successfully, $($ScriptBlockParams.KapeTargetsPath) is missing, exiting."
+        Throw "$($ScriptBlockParams.KapeTargetsPath) is missing."
+        exit
+    } else {
+        WriteLog -Severity "Info" -Message "KAPE ran successfully."
     }
 
     # Create Zip 
-    Write-Host "$(Get-Date) Info Running 7z to compress KAPE outputs."
-    & $ScriptBlockParams.ZipExePath $ScriptBlockParams.ZipArgs
+    WriteLog -Severity "Info" -Message "Attempting to compress KAPE outputs."
+    try {
+        & $ScriptBlockParams.ZipExePath $ScriptBlockParams.ZipArgs
+    } catch {
+        WriteLog -Severity "Error" -Message " $_."
+        exit
+    }
 
     # If zip archive not created, something went wrong; exit
     if (-not($(Test-Path $ScriptBlockParams.KapeOutputsPath))) {
-        exit 1
+        WriteLog -Severity "Error" -Message " Failed to compress KAPE outputs, exiting."
+        exit
+    } else {
+        WriteLog -Severity "Info" -Message "Successfully compressed KAPE outputs to $($ScriptBlockParams.KapeOutputsPath)."
     }
 
-    # Upload to Azure
-    if ($ScriptBlockParams.AzcopyArgs -ne $null) {
-        Write-Host "$(Get-Date) Info Running azcopy to upload $($ScriptBlockParams.KapeOutputsPath) to remote storage."
-        & $ScriptBlockParams.AzcopyExePath $ScriptBlockParams.AzcopyArgs
+    # Upload to Azure with a 300 second timeout
+    if ($null -ne $ScriptBlockParams.AzcopyArgs) {
+        WriteLog -Severity "Info" -Message "Running azcopy to upload $($ScriptBlockParams.KapeOutputsPath) to remote storage."
+    
+        $Job = Start-Job -ScriptBlock {
+            & $using:ScriptBlockParams.AzcopyExePath $using:ScriptBlockParams.AzcopyArgs
+        }
+        $Job | Wait-Job -Timeout 300 | Out-Null
+        if ($Job.State -eq 'Running') {
+            $Job | Stop-Job
+            WriteLog -Severity "Error" -Message "Connection timeout to https://$StorageAccount.blob.core.windows.net/$StorageContainer, exiting."
+            exit
+        } elseif ($Job.State -eq 'Failed') {
+            $ErrorString = $($Job.ChildJobs[0].JobStateInfo.Reason.Message)
+        } elseif ($Job.State -eq 'Completed') {
+            $ErrorString = $Job | Receive-Job | Select-String -Pattern 'RESPONSE 403:|error' | Select-Object -First 1
+        }
+
+        if ($null -ne $ErrorString) {
+            WriteLog -Severity "Error" -Message "Upload to remote storage failed; $ErrorString."
+            exit
+        } else {
+            WriteLog -Severity "Info" -Message "Successfully uploaded KAPE outputs to remote storage."
+        }
     }
 
     # If storing to a remote location, delete local outputs
-    if (($ScriptBlockParams.AzcopyArgs -ne $null) -AND $(Test-Path $ScriptBlockParams.KapeOutputsPath)) {
-        Write-Host "$(Get-Date) Info Removing $($ScriptBlockParams.KapeOutputsPath)"
-        Remove-Item $ScriptBlockParams.KapeOutputsPath -Recurse
+    if (($null -ne $ScriptBlockParams.AzcopyArgs) -AND $(Test-Path $ScriptBlockParams.KapeOutputsPath)) {
+        WriteLog -Severity "Info" -Message "Removing $($ScriptBlockParams.KapeOutputsPath)"
+        Remove-Item $ScriptBlockParams.KapeOutputsPath -Recurse -Force
     }
 
     # Clean up targets 
     if ($(Test-Path $ScriptBlockParams.KapeTargetsPath)) {
-        Write-Host "$(Get-Date) Info Removing $($ScriptBlockParams.KapeTargetsPath)"
-        Remove-Item $ScriptBlockParams.KapeTargetsPath -Recurse
+        WriteLog -Severity "Info" -Message "Removing $($ScriptBlockParams.KapeTargetsPath)"
+        Remove-Item $ScriptBlockParams.KapeTargetsPath -Recurse -Force
     }
 
     # Clean up modules
     if ($(Test-Path $ScriptBlockParams.KapeModulesPath)) {
-        Write-Host "$(Get-Date) Info Removing $($ScriptBlockParams.KapeModulesPath)"
-        Remove-Item $ScriptBlockParams.KapeModulesPath -Recurse
+        WriteLog -Severity "Info" -Message "Removing $($ScriptBlockParams.KapeModulesPath)"
+        Remove-Item $ScriptBlockParams.KapeModulesPath -Recurse -Force
     }
 }
 
@@ -323,30 +399,32 @@ if ($AsBackgroundJob) {
     WriteLog -Severity "Info" -Message "Starting KAPE as background job with args: $KapeArgs"
     
     # Start background job
-    $job = Start-Job -ScriptBlock $ScriptBlock -ArgumentList $ScriptBlockParams
+    $Job = Start-Job -ScriptBlock $ScriptBlock -ArgumentList $ScriptBlockParams
     
     # Wait 10 seconds and check Job has not failed
-    Start-Sleep -Seconds 10
-    if ($job.State -eq 'Failed') {
-        WriteLog -Severity "Error" -Message "Background job failed with error: $($job.ChildJobs[0].JobStateInfo.Reason.Message)"
+    Start-Sleep -Seconds 20
+    if ($Job.State -eq 'Failed') {
+        WriteLog -Severity "Error" -Message "Background job failed with error: $($Job.ChildJobs[0].JobStateInfo.Reason.Message)"
     } else {
+        WriteLog -Severity "Info" -Message "KAPE is running, this may take a couple minutes."
         if ($PSBoundParameters.ContainsKey('StorageAccount')) {
-            WriteLog -Severity "Info" -Message "KAPE is running, outputs will be stored at https://$StorageAccount.blob.core.windows.net/$StorageContainer/$KapeOutputsArchive; This may take a couple minutes."
+            WriteLog -Severity "Info" -Message "Once the job is complete, outputs will be stored at https://$StorageAccount.blob.core.windows.net/$StorageContainer/$KapeOutputsArchive."
         } else {
-            WriteLog -Severity "Info" -Message "KAPE is running, outputs will be stored in $KapeOutputsPath; This may take a couple minutes."
+            WriteLog -Severity "Info" -Message "Once the job is complete, outputs will be stored in $KapeOutputsPath."        
         }
+        WriteLog -Severity "Info" -Message "Once the job is complete, a full console log of this session will be stored within $LOGGING_PATH"
     }
-
 } else {
     WriteLog -Severity "Info" -Message "Starting KAPE with args: $KapeArgs"
 
     # Start in foreground
     & $ScriptBlock -ScriptBlockParams $ScriptBlockParams
     if ($PSBoundParameters.ContainsKey('StorageAccount')) {
-        WriteLog -Severity "Info" -Message "Done; Outputs are stored at https://$StorageAccount.blob.core.windows.net/$StorageContainer/$KapeOutputsArchive."
+        WriteLog -Severity "Info" -Message "Outputs are stored at https://$StorageAccount.blob.core.windows.net/$StorageContainer/$KapeOutputsArchive."
     } else {
-        WriteLog -Severity "Info" -Message "Done; Outputs are stored in $KapeOutputsPath."
+        WriteLog -Severity "Info" -Message "Outputs are stored in $KapeOutputsPath."
     }
+    WriteLog -Severity "Info" -Message "A full console log of this session is stored within $LOGGING_PATH"
 }
 
 # Enable progress bars to write to the standard output
